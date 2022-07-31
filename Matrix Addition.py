857e6301f39558ff04e819a925474512089c9d12
JAIDON GILL SHAJAN <84973988+JUDU-13@users.noreply.github.com>
2022-07-31 13:40:36 +0530

    add

--- Matrix Addition.py
+++ Matrix Addition.py
@@ -1,5 +1,19 @@
 import numpy as np
-a = np.array([[1,2,3],[4,5,6],[7,8,9]])
-b = np.array([[9,8,7],[6,5,4],[3,2,1]])
-s = np.add(a,b)
-print(s)
+rows = int(input("Enter the Number of rows : " ))
+column = int(input("Enter the Number of Columns: "))
+
+print("Enter the elements of First Matrix:")
+m1= [[int(input()) for i in range(column)] for i in range(rows)]
+print("First Matrix is: ")
+for n in m1:
+    print(n)
+m1 = np.array(m1)
+
+print("Enter the elements of Second Matrix:")
+m2= [[int(input()) for i in range(column)] for i in range(rows)]
+print("Second Matrix is: ")
+for n in m1:
+    print(n)
+m2 = np.array(m2)
+
+print("SUM=",m1+m2)
